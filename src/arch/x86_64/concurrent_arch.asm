47b0ce7d9e54eff18371b67c42aa4004fcbe22c1
sharow <shirowmiura@gmail.com>
2016-01-12 18:49:09 +0900

    follow change (x86_64)

--- src/arch/x86_64/concurrent_arch.asm
+++ src/arch/x86_64/concurrent_arch.asm
@@ -10,13 +10,13 @@
 
 section .text
 
-global ConcurrentArch_SetupExecutionContext
-global ConcurrentArch_TrampolineToProcedure
-global ConcurrentArch_TrampolineToCaller
+global concurrent_arch_setup_execution_context
+global concurrent_arch_trampoline_to_procedure
+global concurrent_arch_trampoline_to_caller
 
-global ConcurrentContext_Offsetof_Procedure
-global ConcurrentContext_Offsetof_StackPointer
-global ConcurrentContext_Offsetof_CallerReturnAddress
+global concurrent_offsetof_procedure
+global concurrent_offsetof_stack_ptr
+global concurrent_offsetof_caller_return_addr
 
 %macro save_context 0
     push rbx
@@ -41,20 +41,20 @@ global ConcurrentContext_Offsetof_CallerReturnAddress
 %endmacro
 
 ;;
-;; void ConcurrentArch_SetupExecutionContext(ConcurrentContext *aContext)
+;; void concurrent_arch_setup_execution_context(struct concurrent_ctx *ctx)
 ;;
-ConcurrentArch_SetupExecutionContext:
+concurrent_arch_setup_execution_context:
     mov rcx, rdi
 
     ; exchange stack
-    mov rax, qword [ConcurrentContext_Offsetof_StackPointer]
+    mov rax, qword [concurrent_offsetof_stack_ptr]
     xchg rsp, qword [rcx + rax]
 
-    push rcx                                 ; aContext for "return" or out of scope
-    push rcx                                 ; aContext for yield
-    push ConcurrentArch_ReturnAtProcedure
+    push rcx                                 ; ctx for "return" or out of scope
+    push rcx                                 ; ctx for yield
+    push concurrent_arch_return_at_procedure
 
-    mov rax, qword [ConcurrentContext_Offsetof_Procedure]
+    mov rax, qword [concurrent_offsetof_procedure]
     push qword [rcx + rax] ; entry point
 
     ; initial register value
@@ -69,27 +69,27 @@ ConcurrentArch_SetupExecutionContext:
     push rax  ; r15
 
     ; restore stack
-    mov rax, qword [ConcurrentContext_Offsetof_StackPointer]
+    mov rax, qword [concurrent_offsetof_stack_ptr]
     xchg rsp, qword [rcx + rax]
 
     ret
 
 
 ;;
-;; void ConcurrentArch_TrampolineToProcedure(ConcurrentContext *aContext)
+;; void concurrent_arch_trampoline_to_procedure(struct concurrent_ctx *ctx)
 ;;
-ConcurrentArch_TrampolineToProcedure:
+concurrent_arch_trampoline_to_procedure:
     mov rcx, rdi
 
     save_context
 
     ; save return address
     mov rdx, qword [rsp + 8 * 8]      ; return address of this function
-    mov rax, qword [ConcurrentContext_Offsetof_CallerReturnAddress]
+    mov rax, qword [concurrent_offsetof_caller_return_addr]
     mov qword [rcx + rax], rdx
 
     ; exchange stack
-    mov rax, qword [ConcurrentContext_Offsetof_StackPointer]
+    mov rax, qword [concurrent_offsetof_stack_ptr]
     xchg rsp, qword [rcx + rax]
     nop
 
@@ -99,20 +99,20 @@ ConcurrentArch_TrampolineToProcedure:
 
 
 ;;
-;; void ConcurrentArch_TrampolineToCaller(ConcurrentContext *aContext)
+;; void concurrent_arch_trampoline_to_caller(struct concurrent_ctx *ctx)
 ;;
-ConcurrentArch_TrampolineToCaller:
+concurrent_arch_trampoline_to_caller:
     mov rcx, rdi
 
     save_context
 
     ; exchange stack
-    mov rax, qword [ConcurrentContext_Offsetof_StackPointer]
+    mov rax, qword [concurrent_offsetof_stack_ptr]
     xchg qword [rcx + rax], rsp
     nop
 
     ; get return address
-    mov rax, qword [ConcurrentContext_Offsetof_CallerReturnAddress]
+    mov rax, qword [concurrent_offsetof_caller_return_addr]
     mov rax, qword [rcx + rax]
 
     restore_context
@@ -123,19 +123,19 @@ ConcurrentArch_TrampolineToCaller:
     jmp rax     ; jump to return address
 
 
-ConcurrentArch_ReturnAtProcedure:
+concurrent_arch_return_at_procedure:
     pop rcx   ; Retuen address
-    pop rcx   ; aContext
+    pop rcx   ; ctx
 
     save_context
 
     ; exchange stack
-    mov rax, qword [ConcurrentContext_Offsetof_StackPointer]
+    mov rax, qword [concurrent_offsetof_stack_ptr]
     xchg qword [rcx + rax], rsp
     nop
 
     ; get return address
-    mov rax, qword [ConcurrentContext_Offsetof_CallerReturnAddress]
+    mov rax, qword [concurrent_offsetof_caller_return_addr]
     mov rax, qword [rcx + rax]
 
     restore_context
@@ -147,7 +147,7 @@ ConcurrentArch_ReturnAtProcedure:
 
 
 section .data
+concurrent_offsetof_procedure           dq 0
+concurrent_offsetof_stack_ptr           dq 0
+concurrent_offsetof_caller_return_addr  dq 0
 
-ConcurrentContext_Offsetof_Procedure           dq 0
-ConcurrentContext_Offsetof_StackPointer        dq 0
-ConcurrentContext_Offsetof_CallerReturnAddress dq 0
