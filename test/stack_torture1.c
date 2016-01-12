8e7dba20669b698b929707aef3d0440dea66f8f0
sharow <shirowmiura@gmail.com>
2016-01-12 19:07:02 +0900

    fix test

--- test/stack_torture1.c
+++ test/stack_torture1.c
@@ -1,15 +1,17 @@
 /* -*- Mode: c; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil; -*- */
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <time.h>
 #include <string.h>
 #include <assert.h>
-#include "concurrent/concurrent.h"
-#include "concurrent/short_lower_case_api.h"
+#include <concurrent/concurrent.h>
+#include <concurrent/shortname.h>
 
-void test_generator(ConcurrentContext *aContext)
+
+void test_generator(struct concurrent_ctx *ctx)
 {
-    yield_str(aContext, "pong");
+    yield_value(ctx, "pong");
 }
 
 int main(void)
@@ -19,25 +21,25 @@ int main(void)
     srand(time(NULL));
     concurrent_init();
     for (i = 0; i < 1000; i++) {
-        int stack_size = 1024 + 16 + (rand() % 16);
-        int torture_offset = rand() % 16;
-        ConcurrentContext *context;
-        unsigned char *stack;
-        ConcurrentStatus status;
+        size_t stack_size = 1024 + 16 + (rand() % 16);
+        size_t torture_offset = rand() % 16;
+        struct concurrent_ctx *ctx;
+        uint8_t *stack;
+        concurrent_status status;
 
-        context = malloc(ctx_sizeof());
+        ctx = malloc(ctx_sizeof());
         stack = malloc(sizeof(*stack) * stack_size);
-        status = ctx_construct(context,
+        status = ctx_construct(ctx,
                                stack + torture_offset,
                                stack_size - torture_offset,
                                test_generator, NULL);
 
-        assert(status == ConcurrentStatus_SUCCESS);
-        assert(strcmp(gen_str(context), "pong") == 0);
+        assert(status == CONCURRENT_SUCCESS);
+        assert(strcmp(resume(ctx), "pong") == 0);
 
-        ctx_destruct(context);
+        ctx_destruct(ctx);
         free(stack);
-        free(context);
+        free(ctx);
     }
     concurrent_fin();
     return 0;
